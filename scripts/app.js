97c270d7181964fe632faa77859fb15926245f50
gboivin <boivin.gregoire@gmail.com>
2016-05-16 13:54:34 +0200

    Fix logo path.

--- scripts/app.js
+++ scripts/app.js
@@ -228,7 +228,7 @@ const Header = React.createClass({
         var title = "Europe's most modern bank";
         return(
             <header className={this.props.atm.state.currentStep.view != 'SelectAmount' ? 'center' : ''}>
-                <img  className="svg-logo" src="../images/logo.svg"/>
+                <img  className="svg-logo" src="images/logo.svg"/>
                 <h1>{title}</h1>
             </header>
         );
